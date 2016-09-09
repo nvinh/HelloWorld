0ee54a42c676cb578aec61a1fe482fa275939778
Vincent <vinh.ioit@gmail.com>
2016-09-09 15:00:02 +1200

    first commit

---
+++ HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.WelcomeString = "Thử cái ViewBag xem sao";
+            var message = new HelloWorld.Models.TestMessageModel();
+            message.TestWelcome = "Huhu em ở đây nè ặc ặc.";
+            return View(message);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
